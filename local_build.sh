3ae5ccc69e6a89d715ccc5c68579864e644f9f6f
Karl Schriek <karl.schriek@bma.network>
2022-06-02 15:06:01 +0200

    Do not use locally forked go-autorest, instead bump go-autorest to v0.9.20

--- local_build.sh
+++ local_build.sh
@@ -1,4 +1,9 @@
+# Not, before running this script you must set a GITHUB_TOKEN as environment variable. See https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-container-registry
+
+echo $GITHUB_TOKEN | docker login ghcr.io -u USERNAME --password-stdin
+TAG=$(git describe)
+
 make build
 cp /etc/ssl/certs/ca-certificates.crt ca-certificates.crt
-docker build . -t kschriek/external-dns:latest
-docker push kschriek/external-dns:latest
+docker build . -t ghcr.io/block-markets-africa/external-dns:$TAG
+docker push ghcr.io/block-markets-africa/external-dns:$TAG
